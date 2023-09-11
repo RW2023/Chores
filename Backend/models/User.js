8fd8befec734182cb48b117d1836f54a6ce3c95e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 16:28:45 -0400

    feat: Updated User model to include password and pre-save hook for hashing

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    kids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Kid' }]
+    // other fields like roles, permissions, etc. can go here
+});
+
+// Pre-save hook to hash password before saving a new user
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
