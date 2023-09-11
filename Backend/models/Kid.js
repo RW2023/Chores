ce50f9a0ad540ec0a3970f706494b555d48a2bd9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 10:51:25 -0400

    fix: Corrected syntax error in Kid schema and added gender field

--- Backend/models/Kid.js
+++ Backend/models/Kid.js
@@ -4,7 +4,12 @@ const kidSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
     chores: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chore' }],
-    rewards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward' }]
+    rewards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward' }],
+    gender: { // <-- Add this new field
+        type: String,
+        required: true,  // make it required if you want
+        enum: ['male', 'female', 'other']  // if you want to restrict the values
+    },
 });
 
 module.exports = mongoose.model('Kid', kidSchema);
