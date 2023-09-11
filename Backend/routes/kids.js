5e2eb2156767674fbb9c1e5e31688fc75886b7c8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 04:22:52 -0400

    Implemented CRUD operations in kids.js

---
+++ Backend/routes/kids.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const Kid = require('../models/Kid');
+const router = express.Router();
+
+// Fetch all kids
+router.get('/', async (req, res) => {
+  try {
+    const kids = await Kid.find();
+    res.json(kids);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a new kid
+router.post('/', async (req, res) => {
+  const kid = new Kid({
+    name: req.body.name,
+    age: req.body.age,
+    schoolLevel: req.body.schoolLevel
+  });
+
+  try {
+    const newKid = await kid.save();
+    res.status(201).json(newKid);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Fetch a single kid by ID
+router.get('/:id', getKid, (req, res) => {
+  res.json(res.kid);
+});
+
+// Update a kid by ID
+router.put('/:id', getKid, async (req, res) => {
+  if (req.body.name != null) {
+    res.kid.name = req.body.name;
+  }
+  if (req.body.age != null) {
+    res.kid.age = req.body.age;
+  }
+  if (req.body.schoolLevel != null) {
+    res.kid.schoolLevel = req.body.schoolLevel;
+  }
+  try {
+    const updatedKid = await res.kid.save();
+    res.json(updatedKid);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a kid by ID
+router.delete('/:id', getKid, async (req, res) => {
+  try {
+    await res.kid.remove();
+    res.json({ message: 'Deleted Kid' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware for getKid
+async function getKid(req, res, next) {
+  let kid;
+  try {
+    kid = await Kid.findById(req.params.id);
+    if (kid == null) {
+      return res.status(404).json({ message: 'Cannot find kid' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.kid = kid;
+  next();
+}
+
+module.exports = router;
