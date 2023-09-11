426631f27d97f220eb7ab7628e126895a1b94193
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 04:36:08 -0400

    Implemented CRUD operations in rewards.js

---
+++ Backend/routes/rewards.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const Reward = require('../models/Reward');
+const router = express.Router();
+
+// Fetch all rewards
+router.get('/', async (req, res) => {
+    try {
+        const rewards = await Reward.find();
+        res.json(rewards);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a new reward
+router.post('/', async (req, res) => {
+    const reward = new Reward({
+        name: req.body.name,
+        description: req.body.description,
+        points: req.body.points
+    });
+
+    try {
+        const newReward = await reward.save();
+        res.status(201).json(newReward);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Fetch a single reward by ID
+router.get('/:id', getReward, (req, res) => {
+    res.json(res.reward);
+});
+
+// Update a reward by ID
+router.put('/:id', getReward, async (req, res) => {
+    if (req.body.name != null) {
+        res.reward.name = req.body.name;
+    }
+    if (req.body.description != null) {
+        res.reward.description = req.body.description;
+    }
+    if (req.body.points != null) {
+        res.reward.points = req.body.points;
+    }
+    try {
+        const updatedReward = await res.reward.save();
+        res.json(updatedReward);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete a reward by ID
+router.delete('/:id', getReward, async (req, res) => {
+    try {
+        await res.reward.remove();
+        res.json({ message: 'Deleted Reward' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Middleware for getReward
+async function getReward(req, res, next) {
+    let reward;
+    try {
+        reward = await Reward.findById(req.params.id);
+        if (reward == null) {
+            return res.status(404).json({ message: 'Cannot find reward' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.reward = reward;
+    next();
+}
+
+module.exports = router;
