1983ebe5b415df5a18a38d6fda4c52b3b2c5bacf
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 15:53:40 -0400

    Imported and utilized chore, kid, and reward routes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
-// Import required modules
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');  // Assuming you're using CORS middleware
 
 // Import routes
-const choresRoutes = require('./routes/chores');
-const kidsRoutes = require('./routes/kids');
-const rewardsRoutes = require('./routes/rewards');
+const choreRoutes = require('./routes/chores');
+const kidRoutes = require('./routes/kids');
+const rewardRoutes = require('./routes/rewards');
 
 // Load environment variables
 dotenv.config();
@@ -16,6 +16,7 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());  // Enable CORS if you're using it
 
 // MongoDB URI from .env
 const MONGO_URI = process.env.MONGO_URI;
@@ -32,10 +33,10 @@ mongoose.connect(MONGO_URI, {
         console.error('MongoDB connection error:', err);
     });
 
-// Use imported routes as middleware
-app.use('/api/chores', choresRoutes);
-app.use('/api/kids', kidsRoutes);
-app.use('/api/rewards', rewardsRoutes);
+// Use imported routes
+app.use('/api/chores', choreRoutes);
+app.use('/api/kids', kidRoutes);
+app.use('/api/rewards', rewardRoutes);
 
 // Test Route
 app.get('/', (req, res) => {
@@ -43,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // Listen on port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
