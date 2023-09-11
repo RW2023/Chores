cf62d8f51f8fc866d0a94cbc022a71f6ae406df4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-11 03:40:12 -0400

    setup server

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+// Import required modules
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// MongoDB URI from .env
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
+
+// Test Route
+app.get('/', (req, res) => {
+    res.json({ message: 'The Server is Up Son!' });
+});
+
+// Listen on port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
