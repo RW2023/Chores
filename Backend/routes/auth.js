5f92021b2232c59d4a49d01380ba2e56a5cf84e5
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-12 04:43:31 -0400

    Completed the user registration logic in auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,55 +1,52 @@
-const express = require("express");
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+
 const router = express.Router();
 
-const jwtSecret = process.env.JWT_SECRET || "your-secret-key";
+// User Registration
+router.post(
+    '/register',
+    [
+        // Validate input
+        body('email').isEmail(),
+        body('password').isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-// Sign up a new user
-router.post("/signup", async (req, res) => {
-    try {
+        // Destructure request
         const { email, password } = req.body;
+
+        // Check if user exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ message: 'User already exists' });
         }
 
+        // Hash password and save user
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ email, password: hashedPassword });
-        const savedUser = await newUser.save();
+        const user = new User({
+            email,
+            password: hashedPassword
+        });
+        const savedUser = await user.save();
 
-        const token = jwt.sign({ id: savedUser._id }, jwtSecret);
-        res.status(201).json({ token });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+        // Create JWT token
+        const jwtSecret = process.env.JWT_SECRET;
+        const token = jwt.sign({ id: savedUser._id }, jwtSecret, {
+            expiresIn: '1h'
+        });
+
+        res.json({ token });
     }
-});
-
-// Log in an existing user
-router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-
-        if (!existingUser) {
-            return res.status(400).json({ message: "User does not exist" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(
-            password,
-            existingUser.password
-        );
-
-        if (!isPasswordValid) {
-            return res.status(400).json({ message: "Invalid password" });
-        }
-
-        const token = jwt.sign({ id: existingUser._id }, jwtSecret);
-        res.status(200).json({ token });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+);
 
 module.exports = router;
