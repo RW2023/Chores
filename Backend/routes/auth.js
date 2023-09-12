e9bb2cf90c586a4a0660b9bbb12b863735f6a4fd
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-12 03:44:00 -0400

    fix: Moved JWT token generation into register and login routes in auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,18 +3,27 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const jwtSecret = process.env.JWT_SECRET;
-const token = jwt.sign({ id: savedUser._id }, jwtSecret);
 
 const router = express.Router();
 
 // User Registration
 router.post('/register', async (req, res) => {
     // Your registration logic here
+    // ...
+    // Assume savedUser is the user object returned upon registration
+    const token = jwt.sign({ id: savedUser._id }, jwtSecret);
+    // Send token back to client
+    res.json({ token });
 });
 
 // User Login
 router.post('/login', async (req, res) => {
     // Your login logic here
+    // ...
+    // Assume savedUser is the user object returned upon login
+    const token = jwt.sign({ id: savedUser._id }, jwtSecret);
+    // Send token back to client
+    res.json({ token });
 });
 
 module.exports = router;
