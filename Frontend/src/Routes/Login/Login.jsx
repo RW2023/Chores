2cd1572ab390c335330a742d0d605585c0e15fa3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-12 18:40:29 -0400

    Implemented the Login component with form fields and API call to login endpoint.

--- Frontend/src/Routes/Login/Login.jsx
+++ Frontend/src/Routes/Login/Login.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
-import './Login.css';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        };
+
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, config);
+
+            if (response.status !== 200) {
+                const data = await response.json();
+                console.error(data.error);
+                return;
+            }
+
+            const data = await response.json();
+            localStorage.setItem('authToken', data.token);
+
+            // Redirect to home or dashboard route
+        } catch (error) {
+            console.error('There was an error logging in:', error);
+        }
+    };
+
     return (
-        <div className="login">
-            <h1>Login Page</h1>
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 };
