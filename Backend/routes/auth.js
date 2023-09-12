0d0d5c6805c55d6a78082590fb20ddc871562615
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-12 07:29:46 -0400

    Added missing login logic in auth.js.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,48 +2,80 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const verifyToken = require('../middleware/verifyToken');
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
-const jwtSecret = process.env.JWT_SECRET || 'your_jwt_secret';
+const jwtSecret = process.env.JWT_SECRET;
 
 // Register
-router.post('/register', async (req, res) => {
-    const { email, password, username } = req.body;
+router.post(
+    '/register',
+    [
+        check('email', 'Invalid email').isEmail(),
+        check('password', 'Invalid password').isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    // Check if user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ error: 'User already exists' });
+        const { email, password, username } = req.body;
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
 
-    // Create new user
-    const user = new User({ username, email, password: hashedPassword });
-    const savedUser = await user.save();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            email,
+            password: hashedPassword,
+            username,
+        });
 
-    // Generate JWT token
-    const token = jwt.sign({ id: savedUser._id }, jwtSecret, { expiresIn: '1h' });
+        const savedUser = await user.save();
+        const token = jwt.sign({ id: savedUser._id }, jwtSecret, {
+            expiresIn: 3600, // 1 hour
+        });
 
-    res.json({ token });
-});
+        res.json({ token });
+    }
+);
 
 // Login
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+router.post(
+    '/login',
+    [
+        check('email', 'Invalid email').isEmail(),
+        check('password', 'Invalid password').isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    // Check if user exists
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ error: 'Invalid email or password' });
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
-    // Check password
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) return res.status(400).json({ error: 'Invalid email or password' });
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid email or password' });
+        }
 
-    // Generate JWT token
-    const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Invalid email or password' });
+        }
 
-    res.json({ token });
-});
+        const token = jwt.sign({ id: user._id }, jwtSecret, {
+            expiresIn: 3600, // 1 hour
+        });
+
+        res.json({ token });
+    }
+);
 
 module.exports = router;
