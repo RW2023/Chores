49a7527c3ee1306ee56f39ef96b791ef15f5d6cc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-17 19:37:46 -0400

    Fix: Registration form now working with backend fixed path to api in registration form was set to /register instead of /api/auth/register

--- Frontend/src/forms/RegistrationForm.jsx
+++ Frontend/src/forms/RegistrationForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Button, TextField, Container, Typography } from '@mui/material';
 
 const RegistrationForm = () => {
@@ -8,6 +9,8 @@ const RegistrationForm = () => {
     password: '',
   });
 
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -16,10 +19,30 @@ const RegistrationForm = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Here you can call the API to send the formData
-  };
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch('http://localhost:5000/api/auth/register', {  // replace with your backend URL
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Registration successful:', data);
+      // Navigate to the dashboard or show a success message
+    } else {
+      const data = await response.json();
+      console.error('Registration failed:', data);
+      // Show an error message
+    }
+  } catch (error) {
+    console.error('There was a problem with the registration:', error);
+  }
+};
 
   return (
     <Container>
@@ -58,6 +81,7 @@ const RegistrationForm = () => {
           Register
         </Button>
       </form>
+      {confirmationMessage && <Typography variant="body2">{confirmationMessage}</Typography>}
     </Container>
   );
 };
