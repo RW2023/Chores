a3eb532b95511be43251af326798331adb47bf88
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-17 08:48:28 -0400

    Implemented custom theme setup  using Material-UI and global CSS variables.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import theme from './theme';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 
 import HomePage from './pages/HomePage';
@@ -8,16 +14,18 @@ import ChildPage from './pages/ChildPage';
 
 const App = () => {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/parent" element={<ParentPage />} />
-          <Route path="/child" element={<ChildPage />} />
-          {/* Add more Routes here as needed */}
-        </Routes>
-      </Router>
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/parent" element={<ParentPage />} />
+            <Route path="/child" element={<ChildPage />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ Frontend/src/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+import './index.css';
+
+const theme = createTheme({
+  palette: {
+    overlay: 'rgba(184, 193, 236, 0.5)',
+    background: {
+      default: '#11151A', // Very dark background
+    },
+    primary: {
+      main: '#D9BF77', // Gold for elegance
+    },
+    secondary: {
+      main: '#7F8A9E', // Cool gray
+    },
+    text: {
+      primary: '#E7E7E7', // Light text for contrast
+      secondary: '#B0B7C3', // Slightly darker secondary text
+    },
+    // Add more custom colors here as needed
+    customColor1: {
+      main: '#yourColorCode1',
+    },
+    customColor2: {
+      main: '#yourColorCode2',
+    },
+  },
+});
+
+export default theme;
