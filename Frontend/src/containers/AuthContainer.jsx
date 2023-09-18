f806b2e8f6e17d544e6666844e5d43f6764183af
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-17 23:41:51 -0400

    add home button

--- Frontend/src/containers/AuthContainer.jsx
+++ Frontend/src/containers/AuthContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import LoginForm from '../forms/LoginForm';
 import RegistrationForm from '../forms/RegistrationForm';
 import { Button, Box } from '@mui/material';
@@ -10,8 +11,11 @@ const AuthContainer = () => {
     <div>
       {showLogin ? <LoginForm /> : <RegistrationForm />}
       <Box display="flex" justifyContent="center" mt={3}>
-        <Button variant="contained" color="primary" onClick={() => setShowLogin(!showLogin)}>
+        <Button variant="contained" color="primary" onClick={() => setShowLogin(!showLogin)} style={{ marginRight: '10px' }}>
           {showLogin ? 'Switch to Register' : 'Switch to Login'}
+        </Button>
+        <Button component={Link} to="/" variant="contained" color="secondary">
+          Home
         </Button>
       </Box>
     </div>
