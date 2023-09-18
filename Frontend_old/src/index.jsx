90f9df08faf5266a4012678fd97d11c792f508e6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-17 20:41:44 -0400

    deleted frontend_old. past there now

--- Frontend_old/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'; \nimport ReactDOM from 'react-dom'; \nimport App from './App'; \n\nReactDOM.render(<App />, document.getElementById('root')); import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
